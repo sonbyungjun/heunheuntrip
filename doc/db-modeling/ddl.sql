59d9f01800cc027c8e17aa9f40cf26c05ba498ec
Byungjun Son <saint2030@naver.com>
2019-05-09 15:24:28 +0900

    db재설정

--- doc/db-modeling/ddl.sql
+++ doc/db-modeling/ddl.sql
@@ -125,7 +125,7 @@ CREATE TABLE qna (
   content     TEXT         NOT NULL, -- 문의내용
   qna_pwd     VARCHAR(255) NOT NULL, -- 문의글비밀번호
   cdt         DATETIME     NOT NULL DEFAULT current_timestamp(), -- 작성일
-  vw_cnt      INTEGER      NOT NULL  -- 조회수
+  vw_cnt      INTEGER      NOT NULL DEFAULT 0 -- 조회수
 );
 
 -- 문의사항
