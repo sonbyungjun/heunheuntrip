0e99f77f296067cdc1a348efa1a72812db06e2a1
jeonminhee <yycel1357@naver.com>
2019-05-21 15:14:24 +0900

    류재현, 전민희 - add 폼 작성

--- src/main/webapp/html/qna/add.js
+++ src/main/webapp/html/qna/add.js
@@ -1,20 +1,51 @@
-$(document).ready(function() {
+var category = $('#categorylist').html(),
+    cateGenerator = Handlebars.compile(category);
 
-    $('#heun-header').load('../header.html', function(){
-        $('.heun-header-nav').removeClass('navbar-over absolute-top');
-    });
-    
-    $('#heun-footer').load('../footer.html', function(){
-    });
-
-    $('#summernote').summernote({
-      height: 300,
-      lang: 'ko-KR' // default: 'en-US'
-    });
+$(document).ready(function(){
+  $('#heun-header').load('../header.html', function(){
+    $('.heun-header-nav').removeClass('navbar-over absolute-top');
   });
-
-  var save = function() {
-    var markup = $('.click2edit').summernote('code');
-    console.log(markup);
-    $('.click2edit').summernote('destroy');
+  
+  $('#heun-footer').load('../footer.html', function(){
+  });
+  
+  function loadCategory() {
+    $.getJSON('../../app/json/qna/categorylist', function(obj) {
+      $(cateGenerator(obj)).appendTo('.heun-category');
+      $(document.body).trigger('loaded-cate');
+    }); 
   };
+  
+  loadCategory();
+})
+
+    
+    $(document.body).on('loaded-cate', function() {
+        $('.heun-category > a').on('click', function() {
+          $('#dropdownMenuButton').html($(this).html());
+          $('#dropdownMenuButton').attr('data-no', $(this).attr('data-no'));
+        });
+      })
+
+    $('#add-btn').on('click', function() {
+      $.ajax({
+        url: '../../app/json/qna/add',
+        type: 'POST',
+        data: {
+          categoryNo: $('#dropdownMenuButton').attr('data-no'),
+          title: $('#title').val(),
+          content: $('#conts').val(),
+          password: $('#password').val(),
+          parent: $('#title').attr('data-parent'),
+          order: $('#title').attr('data-order'),
+          step: $('#title').attr('data-step')
+        },
+        dataType: 'json',
+        success: function(response) {
+          location.href = 'index.html';
+        },
+        fail: function(error) {
+          alert('등록 실패!!');
+        }
+      });
+    })
