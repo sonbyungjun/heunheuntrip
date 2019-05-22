4eb5ab28f406feb002f9fb64130532e8f18f618b
jeonminhee <yycel1357@naver.com>
2019-05-22 17:38:34 +0900

    민희 재현 - blog detail UI 수정

--- src/main/webapp/html/blog/view.js
+++ src/main/webapp/html/blog/view.js
@@ -1,97 +1,65 @@
 var param = location.href.split('?')[1],
-    checkout = $('#checkoutlist').html(),
-    checkoutGenerator = Handlebars.compile(checkout);
+  checkout = $('#checkoutlist').html(),
+  checkoutGenerator = Handlebars.compile(checkout);
 
 
-$(document).ready(function(){
-  $('#heun-header').load('../header.html', function(){
+$(document).ready(function () {
+  $('#heun-header').load('../header.html', function () {
     $('.heun-header-nav').removeClass('navbar-over absolute-top');
   });
 
-  $('#heun-footer').load('../footer.html', function(){
+  $('#heun-footer').load('../footer.html', function () {
   });
+
+  if (param) {
+    loadData(param.split('=')[1])
+  }
+
 });
 
 
 
 function loadCheckOut() {
-  $.getJSON('../../app/json/blog/roomCheckOut', function(obj) {
-    
+  $.getJSON('../../app/json/blog/roomCheckOut', function (obj) {
+
     $('#title').attr("data-no", obj.userNo);
     $('#title').attr("data-rno", obj.list[0].rmsNo);
-    
+
     $(checkoutGenerator(obj)).appendTo('.heun-checkout');
     $(document.body).trigger('loaded-checkout');
-  }); 
+  });
 };
 
 
 
 function loadData(no) {
-  $.getJSON("../../app/json/blog/detail?no=" + no, function(data) {
-      $('#fbwogus').attr('data-no', data.no);
-      $('#name').html(data.name);
-      $('h1').html(data.title);
-      $('#cont').html(data.content);
-      $('.tooltip').attr('title', data.rmsAddr + " " + data.rmsDetailAddr);
-      $('#createdDate').html(data.createdDate);
-      $('#rmsName').html(data.rmsName);
-      $('#grade').html(data.grade);
+  $.getJSON("../../app/json/blog/detail?no=" + no, function (data) {
+    $('#no').attr('data-no', data.no);
+    $('#name').html(data.name);
+    $('h1').html(data.title);
+    $('#cont').html(data.content);
+    $('.tooltip').attr('title', data.rmsAddr + " " + data.rmsDetailAddr);
+    $('#createdDate').html(data.createdDate);
+    $('#rmsName').html(data.rmsName);
+    $('#grade').html(data.grade);
   });
-  
+
   $('.drowroom').hide();
 };
 
 
 
+$('#delete-btn').on('click', function () {
 
-/*
-$('#update-btn').on('click', function() {
-  
-  var el = document.querySelectorAll('.update');
-  for(e of el){
-    $('.heun-form').attr('readonly', false);
-  }
-
-  if($('#delete-btn').css("display") != "none") {
-    $('#delete-btn').css("display", "none");
-  } else{
-    updateDate($('#no').val());
-  }
-});
-
-
-
-function updateDate() {
   $.ajax({
-    url: '../../app/json/blog/update',
-    type: 'POST',
-    data: {
-      no: $('#no').val(),      
-      title: $('#title').val(),
-      content: $('#content').val()
-        },
-    dataType: 'json',
-    success: function(response) {
-      location.href = 'index.html';
-    },
-    fail: function(error) {
-      alert('변경 실패!!');
-    }
-  });
-}
-*/
-$('#delete-btn').on('click', function() {
-  console.log($('#fbwogus').attr('data-no'));
-  $.ajax({
-    url: '../../app/json/blog/delete?no=' + $('#fbwogus').attr('data-no'),    
+    url: '../../app/json/blog/delete?no=' + $('#no').attr('data-no'),
     type: 'GET',
     dataType: 'json',
-    success: function(response) {
+    success: function (response) {
       location.href = 'index.html';
-  
+
     },
-    fail: function(error) {
+    fail: function (error) {
       alert('삭제 실패!!');
     }
   });
