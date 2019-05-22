593ff48b65e959381338caf718961ebf3832478f
Lyujaehyeon <fbwogus@gmail.com>
2019-05-22 18:04:14 +0900

    류재현 전민희 - 병합한거 푸쉬

--- src/main/webapp/html/blog/view.js
+++ src/main/webapp/html/blog/view.js
@@ -1,6 +1,5 @@
-var param = location.href.split('?')[1],
-  checkout = $('#checkoutlist').html(),
-  checkoutGenerator = Handlebars.compile(checkout);
+var param = location.href.split('?')[1]
+ 
 
 
 $(document).ready(function () {
@@ -19,22 +18,13 @@ $(document).ready(function () {
 
 
 
-function loadCheckOut() {
-  $.getJSON('../../app/json/blog/roomCheckOut', function (obj) {
-
-    $('#title').attr("data-no", obj.userNo);
-    $('#title').attr("data-rno", obj.list[0].rmsNo);
-
-    $(checkoutGenerator(obj)).appendTo('.heun-checkout');
-    $(document.body).trigger('loaded-checkout');
-  });
-};
 
 
 
 function loadData(no) {
   $.getJSON("../../app/json/blog/detail?no=" + no, function (data) {
     $('#no').attr('data-no', data.no);
+    $('#no').attr('data-title', data.title);
     $('#name').html(data.name);
     $('h1').html(data.title);
     $('#cont').html(data.content);
@@ -43,9 +33,55 @@ function loadData(no) {
     $('#rmsName').html(data.rmsName);
     $('#grade').html(data.grade);
   });
-
   $('.drowroom').hide();
 };
+
+
+
+$('#update-btn').on('click', function() {
+
+  $('#add-btn').hide();
+  $('h1').contents().unwrap().wrap( '<textarea id="update-title"></textarea>' );
+  
+  $('.update-content').contents().unwrap().wrap( '<div id="summernote"></div>' );
+ 
+  $('#summernote').summernote({  //썸머노트 활성화 시작
+    placeholder: 'Hello bootstrap 4',
+    tabsize: 2,
+    height: 400
+  });
+ 
+  if($('#delete-btn').css("display") != "none") {
+      $('#delete-btn').css("display", "none");
+      
+  } else{
+    updateDate();
+  }
+
+});
+
+
+
+function updateDate() {
+  var markupStr = $('#summernote').summernote('code');
+  
+  $.ajax({
+    url: '../../app/json/blog/update',
+    type: 'POST',
+    data: {
+      no: $('#no').attr('data-no'),      
+      title: $('#update-title').val(),
+      content: markupStr
+        },
+    dataType: 'json',
+    success: function(response) {
+      location.href = 'index.html';
+    },
+    fail: function(error) {
+      alert('변경 실패!!');
+    }
+  });
+}
 
 
 
