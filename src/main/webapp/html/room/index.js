19c6c791f114143b023b1b30dba8939d9a595af8
Byungjun Son <saint2030@naver.com>
2019-05-22 10:18:25 +0900

    손병준 - 정규표현식 패턴 검사 테스트 코드

--- src/main/webapp/html/room/index.js
+++ src/main/webapp/html/room/index.js
@@ -1,34 +1,20 @@
-var tbody = $('.item-listing'),
-    templateSrc = $('#list-template').html(),
-    trGenerator = Handlebars.compile(templateSrc);
-
-$(document).ready( function() {
-  $('#toggle-filters').sidr({
-    side: 'left',
-    displace: false,
-    renaming: false,
-    name: 'sidebar',
-    source: function () {
-      AOS.refresh();
-    },
-
+$(document).ready(function() {
+	$('#toggle-filters').sidr({
+		side: 'left',
+		displace : false,
+		renaming : false,
+		name: 'sidebar',
+		source: function() {
+		  AOS.refresh();
+		},
   });
-  $("#heun-header").load("/heunheuntrip/html/header.html", function() {
-    $(".heun-header-nav").removeClass("navbar-over absolute-top");
-  });
-  $("#heun-footer").load("/heunheuntrip/html/footer.html");
+  
+  $('#heun-header').load('../header.html', function() {
+    $('.heun-header-nav').removeClass('navbar-over absolute-top');
+  })
+  $('#heun-footer').load('../footer.html')
+
+
+
 });
 
-loadList(1);
-
-function loadList(pn) {
-
-  $.getJSON('../../app/json/room/list', function(obj) {
-    console.log(obj);
-    pageNo = obj.pageNo;
-    tbody.html('');
-    $(trGenerator(obj)).appendTo(tbody);
-    
-  });
-
-} 
