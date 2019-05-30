0163aef01234b413e7dadf48718b409774322828
sijin-kim <tlwls456@naver.com>
2019-05-30 09:58:33 +0900

    -시진 - 수정

--- src/main/webapp/html/auth/signin.js
+++ src/main/webapp/html/auth/signin.js
@@ -39,3 +39,17 @@ document.querySelector('#login-btn').onclick = () => {
   };
   
 };
+
+
+
+var naverLogin = new naver.LoginWithNaverId(
+		{
+			clientId: "9GsCl1_fOwukSDhiTmng",
+			callbackUrl: "http://localhost:8080/heunheuntrip/html/auth/callback.html",
+			isPopup: false, /* 팝업을 통한 연동처리 여부 */
+			loginButton: {color: "green", type: 3, height: 60} /* 로그인 버튼의 타입을 지정 */
+		}
+	);
+	
+	/* 설정정보를 초기화하고 연동을 준비 */
+	naverLogin.init();
