b9b42d5cbd093a7f1a17318b5f9dd27b5a1392e1
Byungjun Son <saint2030@naver.com>
2019-06-03 15:47:41 +0900

    손병준 - 리스트 검색 기능 추가

--- src/main/java/com/heun/trip/service/RoomService.java
+++ src/main/java/com/heun/trip/service/RoomService.java
@@ -4,8 +4,8 @@ import java.util.List;
 import com.heun.trip.domain.Room;
 
 public interface RoomService {
-  List<Room> list(int pageNo, int pageSize);
-  int size();
+  List<Room> list(int pageNo, int pageSize, String lati, String longi);
+  int size(String lati, String longi);
   Room get(int no);
   int add(Room room);
 }
