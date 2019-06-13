dd23e80d49c1f6e9ea72efca7a0709f1a5eeb323
Byungjun Son <saint2030@naver.com>
2019-06-13 17:22:15 +0900

    손병준 - aws s3 초안 설정2

--- src/main/java/com/heun/trip/web/json/AwsFileController.java
+++ src/main/java/com/heun/trip/web/json/AwsFileController.java
@@ -13,6 +13,7 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
@@ -23,7 +24,6 @@ public class AwsFileController {
 
   @PostMapping("upload")
   public void upload(MultipartFile[] files) {
-    System.out.println(files);
     Region region = Region.AP_NORTHEAST_2;
     S3Client s3 = S3Client.builder().region(region).build();
 
@@ -38,18 +38,34 @@ public class AwsFileController {
     }
     System.out.println("버킷에 파일 업로드 완료!");
   }
-  
-  @GetMapping("{name}")
+
+  @GetMapping("down/{name}")
   public void download(@PathVariable String name, OutputStream out) {
     System.out.println(name);
     Region region = Region.AP_NORTHEAST_2;
     S3Client s3 = S3Client.builder().region(region).build();
 
-    s3.getObject(GetObjectRequest.builder()
-        .bucket("b1.sbj.kr").key(name).build(),
-        ResponseTransformer.toOutputStream(out));
-    
+    try {
+      s3.getObject(GetObjectRequest.builder()
+          .bucket("b1.sbj.kr").key(name).build(),
+          ResponseTransformer.toOutputStream(out));
+    } catch (Exception e) {
+      
+      e.printStackTrace();
+    }
     System.out.println("버킷의 파일 다운로드 완료!");
+  }
+  
+  @GetMapping("del/{name}")
+  public void delete(@PathVariable String name) {
+    Region region = Region.AP_NORTHEAST_2;
+    S3Client s3 = S3Client.builder().region(region).build();
+
+    DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+        .bucket("b1.sbj.kr").key(name).build();
+    s3.deleteObject(deleteObjectRequest);
+
+    System.out.println("버킷의 파일 삭제!");
   }
 
 }
