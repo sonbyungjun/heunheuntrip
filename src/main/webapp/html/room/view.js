26be9bcfe6584bf0edec95941c2674ea2c4fa9f2
Byungjun Son <saint2030@naver.com>
2019-06-18 16:46:43 +0900

    손병준 - 결제 프로세스 버그 수정

--- src/main/webapp/html/room/view.js
+++ src/main/webapp/html/room/view.js
@@ -787,6 +787,8 @@ $('body').on('loaded-list', function () {
 			buyer_name: buyer_name,
 			buyer_tel: buyer_tel
 		},  function(rsp) {
+			var isVaild = true;
+			var msg;
 			if (rsp.success) {
 				//[1] 서버단에서 결제정보 조회를 위해 jQuery ajax로 imp_uid 전달하기
 				$.ajax({
@@ -803,43 +805,34 @@ $('body').on('loaded-list', function () {
 				}).done(function(data) {
 					//[2] 서버에서 REST API로 결제정보확인 및 서비스루틴이 정상적인 경우
 					if (data.status === "success") {
-						var msg = '결제가 완료되었습니다.';
-	
-						Swal.fire({
-							type: 'success',
-							title: msg
-						}).then((result) => {
-							if (result.value) {
-								location.href = '/heunheuntrip/html/member/list.html';
-								return;
-							}
-						})
+						isVaild = true;
+						msg = '결제가 완료되었습니다.';
 					} else {
 						//[3] 아직 제대로 결제가 되지 않았습니다.
 						//[4] 결제된 금액이 요청한 금액과 달라 결제를 자동취소처리하였습니다.
-						Swal.fire({
-							type: 'error',
-							title: '이미 예약된 방이거나 결제가 이뤄지지 않았습니다.'
-						}).then((result) => {
-							if (result.value) {
-								return;
-							}
-						})
+						isVaild = false;
+						msg = '이미 예약된 방이거나 결제가 이뤄지지 않았습니다.';
 					}
-				});
-			} else {
-					var msg = '결제에 실패하였습니다.';
-					msg += '\n에러내용 : ' + rsp.error_msg;
+				}).always(function() {
 					Swal.fire({
-						type: 'error',
+						type: isVaild ? 'success' : 'error',
 						title: msg
 					}).then((result) => {
 						if (result.value) {
+							location.href =  isVaild ? '/heunheuntrip/html/member/list.html' : '';
 							return;
 						}
 					})
+				});
+
+
+			} else {
+					var isVaild = false;
+					var msg = '결제에 실패하였습니다.';
+					msg += '\n에러내용 : ' + rsp.error_msg;
 			}
-	})}
+	})
+}
 
 
 	function getUser(cb) {
