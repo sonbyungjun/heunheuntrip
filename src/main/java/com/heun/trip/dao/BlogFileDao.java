49d8a847f5425888915bfd962793ad270621662f
Byungjun Son <saint2030@naver.com>
2019-08-08 23:15:56 +0900

    버그 수정

--- src/main/java/com/heun/trip/dao/BlogFileDao.java
+++ src/main/java/com/heun/trip/dao/BlogFileDao.java
@@ -6,5 +6,5 @@ import com.heun.trip.domain.BlogFile;
 public interface BlogFileDao {
   int insert(List<BlogFile> blogFiles);
   int delete(int no);
-  List<BlogFile> findByNo(int no);
+  List<BlogFile> findByNo(int no); 
 }
